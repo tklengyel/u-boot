b69d8ab94617edb4a96ef64bd79e40466e153827
Homin Lee <suapapa@insignal.co.kr>
2013-10-18 18:23:04 +0900

    Import from SMDK5420 Android 4.2.2 PSP, Revision 1.00, JULY 2013
    Signed-off-by: Homin Lee <suapapa@insignal.co.kr>

--- common/main.c
+++ common/main.c
@@ -372,7 +372,7 @@ void main_loop (void)
 	}
 	else
 #endif /* CONFIG_BOOTCOUNT_LIMIT */
-		s = getenv ("bootcmd");
+	s = getenv ("bootcmd");
 
 	debug ("### main_loop: bootcmd=\"%s\"\n", s ? s : "<UNDEFINED>");
 
